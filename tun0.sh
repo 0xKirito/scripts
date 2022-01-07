a4188258a74fe7dffba85b1c31a4157de888b9fe
0xKirito <40525297+0xKirito@users.noreply.github.com>
2022-01-07 06:11:14 +0530

    generic monitor tun0 IP

---
+++ tun0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# /sbin/ip -o -4 addr list tun0 | awk '{print $4}' | cut -d/ -f1
+# [ -d /proc/sys/net/ipv4/conf/tun0 ] && echo "$({ ip -4 -br a sh dev tun0 | awk {'print $3'} | cut -f1 -d/;} 2>/dev/null) "
+TUN0IP=$((ifconfig tun0 | awk '/inet /{print $2}') 2>/dev/null)
+# TUN0IP=$((/sbin/ip -o -4 addr list tun0 | awk '{print $4}' | cut -d/ -f1) 2>/dev/null);
+if [[ ! -z "$TUN0IP" ]];
+then 
+    # echo "$({ ip -4 -br a sh dev tun0 | awk {'print $3'} | cut -f1 -d/; } 2>/dev/null) "
+    echo "$TUN0IP "
+else 
+    echo "disconnected "
+fi
+
