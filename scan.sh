799505115c7fe22585bb109dba49b74edad87170
0xKirito <40525297+0xKirito@users.noreply.github.com>
2022-01-17 14:35:19 +0530

    nmap automation

--- scan.sh
+++ scan.sh
@@ -13,10 +13,10 @@ NC='\033[0m' # No Color
 ext=".txt"
 timestamp="_$(date "+%Y%m%d-%H.%M.%S")";
 IP=$1
-fast="fast_$IP$timestamp$ext"
-fastsvc="fast_svc_$IP$timestamp$ext"
-slow="slow_$IP$timestamp$ext"
-slowsvc="slow_svc_$IP$timestamp$ext"
+fast="tcp_fast_$IP$timestamp$ext"
+fastsvc="tcp_fast_svc_$IP$timestamp$ext"
+slow="tcp_slow_$IP$timestamp$ext"
+slowsvc="tcp_slow_svc_$IP$timestamp$ext"
 udptop1000="udp_top_1000_$IP$timestamp$ext"
 udptop1000svc="udp_top_1000_svc_$IP$timestamp$ext"
 udpall="udp_all_ports_$IP$timestamp$ext"
@@ -33,41 +33,42 @@ then
     exit
 else
     printf "%s\n\n${WHITE}     =-=-=-=-=-=-=-=-=-=- TCP Scan -=-=-=-=-=-=-=-=-=-=${NC}\n\n"
-    printf "%s\n${LGREEN}Initiating Fast Nmap All TCP Ports Scan... ${NC}\n\n"
-    printf "${WHITE}nmap -Pn -p 0-65535 --min-rate 2000 -v -oN $fast $IP${NC}%s\n\n"
 
-    nmap -Pn -p 0-65535 --min-rate 3000 -v -oN $fast $IP && 
+    printf "%s\n${LGREEN}Initiating Fast Nmap All TCP Ports Scan... ${NC}\n\n"
+    printf "${WHITE}nmap -Pn -p 0-65535 --min-rate 2000 --open -v -oN $fast $IP${NC}%s\n\n"
+
+    nmap -Pn -p 0-65535 --min-rate 2000 --open -v -oN $fast $IP && 
         fastports=$(cat $fast | grep '^[0-9]' | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//);
         # fastports=$(cat $fast | grep '^[0-9]' | cut -d '/' -f 1 | tr '\n' ' ' | sed s/,$//);
 
-    printf "%s\n${LGREEN}Fast Scan => Open Ports: $fastports ${NC}"
-    printf "%s\n\n${LGREEN}Fast Scan => Initiating Service Detection & Scripts Scan... ${NC}\n\n"
+    printf "%s\n${LGREEN}TCP Fast Scan => Open Ports: $fastports ${NC}"
+    printf "%s\n\n${LGREEN}TCP Fast Scan => Initiating Service Detection & Scripts Scan... ${NC}\n\n"
 
-    printf "${WHITE}nmap -Pn -A -p $fastports -v -oN $fastsvc $IP${NC}%s\n\n"
+    printf "${WHITE}nmap -Pn -p $fastports --version-all -A --osscan-guess -v -oN $fastsvc $IP${NC}%s\n\n"
 
-    nmap -Pn -A -p $fastports -v -oN $fastsvc $IP
+    nmap -Pn -p $fastports --version-all -A --osscan-guess -v -oN $fastsvc $IP
 
     printf "%s\n${LGREEN}Initiating Slow Nmap All TCP Ports Scan... ${NC}\n\n"
-    printf "${WHITE}nmap -Pn -p 0-65535 -v -oN $slow $IP${NC}%s\n\n"
+    printf "${WHITE}nmap -Pn -p 0-65535 --open -v -oN $slow $IP${NC}%s\n\n"
 
-    nmap -Pn -p- -v -oN $slow $IP && 
+    nmap -Pn -p 0-65535 --open -v -oN $slow $IP && 
         slowports=$(cat $slow | grep '^[0-9]' | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//);
 
-    printf "%s\n${LGREEN}Fast Scan => Open Ports: $fastports ${NC}"
-    printf "%s\n${LGREEN}Slow Scan => Open Ports: $slowports ${NC}"
+    printf "%s\n${LGREEN}TCP Fast Scan => Open Ports: $fastports ${NC}"
+    printf "%s\n${LGREEN}TCP Slow Scan => Open Ports: $slowports ${NC}"
 
     if [[ "$fastports" != "$slowports" ]]
     then
-        printf "%s\n\n${LGREEN}Slow Scan => Initiating Service Detection & Scripts Scan... ${NC}\n\n"
-        printf "${WHITE}nmap -Pn -A -p $slowports -v -oN $slowsvc $IP${NC}%s\n\n"
-        nmap -Pn -A -p $slowports -v -oN $slowsvc $IP
+        printf "%s\n\n${LGREEN}TCP Slow Scan => Initiating Service Detection & Scripts Scan... ${NC}\n\n"
+        printf "${WHITE}nmap -Pn -p $slowports --version-all -A --osscan-guess -v -oN $slowsvc $IP${NC}%s\n\n"
+        nmap -Pn -p $slowports --version-all -A --osscan-guess -v -oN $slowsvc $IP
 
     elif [[ "$fastports" == "$slowports" ]]
     then
         printf "%s\n\n${LGREEN}No new ports found in Slow Nmap Scan... ${NC}\n"
     fi
 
-    # UDP Scans 
+# UDP Scans 
 
     printf "%s\n\n${WHITE}     =-=-=-=-=-=-=-=-=-=- UDP Scan -=-=-=-=-=-=-=-=-=-=${NC}\n\n"
 
@@ -83,8 +84,8 @@ else
     else
         printf "%s\n${LGREEN}UDP Top 1000 Ports Scan => Open Ports: $udp1000openports ${NC}\n\n"
         printf "%s${LGREEN}Initiating Service Detection and Scripts Scan on Open UDP Ports...${NC}\n\n"
-        printf "%s${WHITE}sudo nmap -Pn -sU -A -p $udp1000openports -oN $udptop1000svc $IP${NC}\n\n"
-        sudo nmap -Pn -sU -A -p $udp1000openports -v -oN $udptop1000svc $IP
+        printf "%s${WHITE}sudo nmap -Pn -sU -p $udp1000openports --version-all -A -v -oN $udptop1000svc $IP${NC}\n\n"
+        sudo nmap -Pn -sU -p $udp1000openports --version-all -A -v -oN $udptop1000svc $IP
 
     fi    
 
@@ -100,7 +101,7 @@ else
     else
         printf "%s\n${LGREEN}UDP All Ports Scan => Open Ports: $udpallopenports ${NC}\n\n"
         printf "%s\n${LGREEN}Initiating Service Detection and Scripts Scan on Open UDP Ports... ${NC}\n\n"
-        printf "%s\n${WHITE}sudo nmap -Pn -sU -A -p $udpallopenports -v -oN $udpallsvc $IP${NC}\n\n"
-        sudo nmap -Pn -sU -A -p $udpallopenports -v -oN $udpallsvc $IP
+        printf "%s\n${WHITE}sudo nmap -Pn -sU -p $udpallopenports --version-all -A -v -oN $udpallsvc $IP${NC}\n\n"
+        sudo nmap -Pn -sU -p $udpallopenports --version-all -A -v -oN $udpallsvc $IP
     fi
 fi
